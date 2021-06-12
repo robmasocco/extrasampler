5b8ebb85b76648332f2c4e99adb89131aead92dc
Roberto Masocco <robmasocco@gmail.com>
2021-06-12 22:09:15 +0200

    Added first tester.

---
+++ Tests/compile_test.cpp
@@ -0,0 +1,19 @@
+/**
+ * @brief Small tester to check that compilation works. Compile with all source files.
+ *
+ * @author Roberto Masocco <robmasocco@gmail.com>
+ *
+ * @date June 12, 2021
+ */
+
+#include <iostream>
+
+#include "../include/extrasampler_linear.hpp"
+#include "../include/extrasampler_quadratic_fixed-time.hpp"
+
+/* The works. */
+int main(void)
+{
+    std::cout << "It compiles!" << std::endl;
+    exit(EXIT_SUCCESS);
+}
